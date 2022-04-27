c3fc2e6a41463346402f114d91775f535369ffa3
Rada1242 <d19125768@mytudublin.ie>
2022-04-27 16:51:46 +0100

    Added class and constructors/methods in Nematode.java

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,79 @@
+package ie.tudublin;
+
+import org.w3c.dom.NameList;
+
+import processing.data.TableRow;
+
+public class Nematode {
+
+    private String name;
+    private int length;
+    private int limbs;
+    private char gender;
+    private int eyes;
+
+    public String toString() {
+        return "Nematode {name: " + name + " length: " + length + "limbs: " + limbs +
+                " gender: " + gender + " eyes : " + eyes;
+    }
+    /*
+    public Nematode(TableRow r)
+    {      
+        tr.getString("name");
+        tr.getInt("length");
+        tr.getInt("limbs");
+        tr.getChar("gender");
+        tr.getInt("eyes");
+    }
+    */
+
+    public Nematode(String name, int length, int limbs, char gender, int eyes)
+    {
+        this.name = name;
+        this.length = length;
+        this.limbs = limbs;
+        this.gender = gender;
+        this.eyes = eyes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLimbs() {
+        return limbs;
+    }
+
+    public void setLimbs(int limbs) {
+        this.limbs = limbs;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+}
+
